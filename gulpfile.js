dab37a3d264f34e9acd3a22696fa74d0b6fb3b9d
T.Hrytsaienko <kortatiana89@gmail.com>
2018-01-19 11:17:34 +0200

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var concat = require('gulp-concat');
+var gcmq = require('gulp-group-css-media-queries');
+var processors = [
+    autoprefixer({
+        browsers: ['last 10 version']
+    })
+];
+
+
+const ignorePug = [
+    '!src/layouts/**',
+    '!src/blocks/**',
+    '!src/globals/**'
+];
+
+gulp.task('html', function () {
+    return gulp.src(['src/**/*.pug', ...ignorePug])
+        .pipe(pug())
+        .pipe(gulp.dest('docs'))
+});
+
+
+gulp.task('js', function () {
+    return gulp.src('src/assets/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('docs/assets'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('src/assets/*.styl')
+        .pipe(stylus().on('error', function (errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        }))
+        .pipe(postcss(processors))
+        .pipe(gcmq())
+        .pipe(gulp.dest('docs/assets'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('vendor:css', function () {
+    return gulp.src('src/vendor/css/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('docs/assets'));
+});
+
+gulp.task('vendor:js', function () {
+    return gulp.src('src/vendor/js/*.js')
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('docs/assets'));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./docs"
+        }
+    });
+});
+
+var reload = function (done) {
+    browserSync.reload();
+    done();
+}
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.pug', gulp.series('html', reload));
+    gulp.watch('src/**/*.styl', gulp.series('css'));
+    gulp.watch('src/**/*.js', gulp.series('js', reload));
+});
+
+gulp.task('copy', function () {
+    return gulp.src([
+            'src/assets/**/*.{jpg,png,jpeg,gif}'
+        ])
+        .pipe(gulp.dest('docs/assets'))
+});
+
+gulp.task('clean', function () {
+    return del('docs');
+});
+
+gulp.task('build', gulp.parallel('html', 'css', 'js', 'copy', 'vendor:css', 'vendor:js'));
+gulp.task('start', gulp.parallel('watch', 'serve'));
+
+gulp.task('default', gulp.series('clean', 'build', 'start'));
